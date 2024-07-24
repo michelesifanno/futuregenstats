8063842cbbd88b705c96872f1debc18c5565f113
Michele Sifanno <michelesifanno@Air-di-Michele.homenet.telecomitalia.it>
2024-07-24 23:04:47 +0200

    image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-          <img src={img.jpeg} className="logo" alt="Vite logo" />
+          <img src={"/img.jpeg"} className="logo" alt="Vite logo" />
       </div>
     </>
   )
