f37fa05bfd4caf0ac2f281d5f3080dbf2be7a7a0
Michele Sifanno <michelesifanno@Air-di-Michele.homenet.telecomitalia.it>
2024-08-05 18:39:11 +0200

    Header Grafica Def

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import { AppBar, Box, InputBase, Grid, Typography, Button } from '@mui/material';
+import { styled, useTheme } from '@mui/material/styles';
+import { AppBar, Box, InputBase, Grid, Typography, Button, Drawer, IconButton, useMediaQuery } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import CloseIcon from '@mui/icons-material/Close';
 import useCompetitions from '../utils/useCompetitions';
 
+const drawerWidth = '100%';
 
 const logoMap = {
     'Premier League': '/premier-league.png',
@@ -15,7 +17,6 @@ const logoMap = {
     'Brasileirão': '/brasilerao.png',
     'Primeira Liga': '/primeira-liga.png',
 };
-
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -37,16 +38,14 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    color:theme.palette.third.main
+    color: theme.palette.third.main,
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: 'third.main',
-    padding: '5px',
     width: '100%',
     '& .MuiInputBase-input': {
         padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
         transition: theme.transitions.create('width'),
         [theme.breakpoints.up('sm')]: {
@@ -58,32 +57,52 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
 }));
 
-
 export default function AppNavbar() {
-    const { competitions, error, loading } = useCompetitions(); // Usa l'Hook qui
+    const theme = useTheme();
+    const { competitions, error, loading } = useCompetitions();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
 
-    console.log("Competitions:", competitions); // Aggiungi questo log per verificare i dati
+    const toggleDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+    };
+
+    const handleDrawerClose = () => {
+        setDrawerOpen(false);
+    };
 
     return (
-        <Box sx={{ flexGrow: 1, position: 'fixed' }}>
+        <Box sx={{ flexGrow: 1, position: 'fixed', width: '100%' }}>
             <AppBar sx={{ alignItems: 'center' }}>
                 <Grid container spacing={2} alignItems="center" sx={{ padding: '20px' }}>
                     <Grid item xs sx={{ display: 'flex', justifyContent: 'start', alignItems: 'center' }}>
-                        <img src="/logo.png" alt="Future Gen Stats Logo" width="55%" />
+                        <img src="/logo.png" alt="Future Gen Stats Logo" style={{ maxWidth: '200px' }} />
                     </Grid>
-                    <Grid item xs={6}>
-                        <Search>
-                            <SearchIconWrapper>
-                                <SearchIcon />
-                            </SearchIconWrapper>
-                            <StyledInputBase
-                                placeholder="Search…"
-                                inputProps={{ 'aria-label': 'search' }}
-                            />
-                        </Search>
-                    </Grid>
+                    {!isMobile && (
+                        <Grid item xs={6}>
+                            <Search>
+                                <SearchIconWrapper>
+                                    <SearchIcon />
+                                </SearchIconWrapper>
+                                <StyledInputBase
+                                    placeholder="Search Player"
+                                    inputProps={{ 'aria-label': 'search' }}
+                                />
+                            </Search>
+                        </Grid>
+                    )}
                     <Grid item xs sx={{ display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
-                        <Button
+                        {isMobile && (
+                            <IconButton color="inherit" onClick={toggleDrawer}>
+                                <SearchIcon />
+                            </IconButton>
+                        )}
+                        {isMobile ? (
+                            <IconButton color="inherit" onClick={toggleDrawer}>
+                                <EmojiEventsIcon />
+                            </IconButton>
+                        ) : (
+                            <Button
                             endIcon={<EmojiEventsIcon />}
                             sx={{
                                 borderColor: '#fff',
@@ -92,14 +111,46 @@ export default function AppNavbar() {
                                     borderColor: '#fff',
                                     backgroundColor: '#2130a2',
                                 },
-                                fontSize:'16px'
+                                fontSize: '16px',
                             }}
                         >
                             Best Under23
                         </Button>
+                        )}
                     </Grid>
                 </Grid>
-                <Grid container spacing={0} alignItems="center" sx={{ backgroundColor: 'third.main' }}>
+                {/* Drawer per la barra di ricerca mobile */}
+                <Drawer
+                    anchor="top"
+                    open={drawerOpen}
+                    onClose={handleDrawerClose}
+                    sx={{
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            padding: '10px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            boxSizing: 'border-box',
+                            backgroundColor: '#fff',
+                        },
+                    }}
+                >
+                    <IconButton color="inherit" onClick={handleDrawerClose}>
+                        <CloseIcon />
+                    </IconButton>
+                    <Search sx={{ width: '100%' }}>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search Player"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+                </Drawer>
+
+                {/* Sezione loghi competizioni */}
+                <Grid container spacing={0} alignItems="center" sx={{ backgroundColor: theme.palette.third.main }}>
                     {loading && <p>Loading...</p>}
                     {error && <p>Error: {error}</p>}
                     {!loading && !error && competitions.length === 0 && <p>No competitions found</p>}
@@ -114,14 +165,23 @@ export default function AppNavbar() {
                                 justifyContent: 'center',
                                 borderRight: index < competitions.length - 1 ? '1px solid #f8f8f81c' : 'none',
                                 '&:hover': {
-                                    backgroundColor: '#2130a2', // Black background on hover
-                                    color: '#fff', // White text color on hover
+                                    backgroundColor: '#2130a2',
+                                    color: '#fff',
                                 },
-                                transition: 'background-color 0.3s ease', // Smooth transition for hover effect
+                                transition: 'background-color 0.3s ease',
                             }}
                         >
-                            <img src={logoMap[competition]} alt={competition} width="30px" height="30px" style={{ marginRight: '5px', padding: '5px' }} />
-                            <Typography sx={{ fontSize: '10px', textTransform: 'uppercase', fontWeight: '500' }}>{competition}</Typography>
+                            <img src={logoMap[competition]} alt={competition} width="30px" height="30px" style={{ marginRight: isMobile ? '0px' : '5px', padding: '5px' }} />
+                            <Typography
+                                sx={{
+                                    fontSize: '10px',
+                                    textTransform: 'uppercase',
+                                    fontWeight: '500',
+                                    display: isMobile ? 'none' : 'block',
+                                }}
+                            >
+                                {competition}
+                            </Typography>
                         </Grid>
                     ))}
                 </Grid>
