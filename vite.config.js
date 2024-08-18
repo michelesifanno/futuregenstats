6ce851fe436dff1876ddaa00e018769d85672371
Michele Sifanno <michelesifanno@Air-di-Michele.homenet.telecomitalia.it>
2024-08-18 11:30:00 +0200

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Configurazione per gestire il fallback al file index.html
+  server: {
+    historyApiFallback: true,
+  },
+});
