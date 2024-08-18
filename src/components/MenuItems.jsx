445a3d36ef5e6db90bd690f0e46fe18c31a1fc80
Michele Sifanno <michelesifanno@Air-di-Michele.homenet.telecomitalia.it>
2024-08-18 17:55:59 +0200

    menu

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,8 +1,10 @@
-// src/components/MenuItems.js
+// src/components/MenuAccordion.js
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { List, ListItemButton, Divider, Typography } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails, Typography, List, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { SportsSoccer, People, School, Star, AccessAlarm, TrendingUp } from '@mui/icons-material';
 import { styled } from '@mui/material/styles';
 
 // Styled components
@@ -10,59 +12,105 @@ const MenuLink = styled(Link)(({ theme }) => ({
     textDecoration: 'none',
 }));
 
-const MenuItemText = styled(Typography)(({ theme }) => ({
-    fontFamily: 'Bai Jamjuree, sans-serif',
-    fontSize: '30px',
-    fontWeight: 600,
-    color: '#fff',
-    textTransform: 'uppercase',
-    margin: 0,
-    letterSpacing: '-0.5px',
-    transition: 'color 0.3s', // Smooth color transition
+const MenuItemText = styled(ListItemText)(({ theme }) => ({
+    '& .MuiTypography-root': {
+        fontSize: '16px',
+        fontWeight: 600,
+        color: '#fff',
+        textTransform: 'uppercase',
+        margin: 0,
+        letterSpacing: '-0.5px',
+        transition: 'color 0.3s',
+    },
 }));
 
 const StyledListItemButton = styled(ListItemButton)(({ theme }) => ({
     '&:hover': {
         backgroundColor: 'transparent',
     },
-    '&:hover .menu-item-text': {
-        color: '#00e8da', // Change text color on hover
+    '&:hover .MuiTypography-root': {
+        color: '#00e8da',
+    },
+    '&:hover .MuiListItemIcon-root': {
+        color: '#00e8da',
     },
 }));
 
-export default function MenuItems() {
+const accordionStyles = {
+    boxShadow: 'none',
+    border: '1px solid rgba(0, 0, 0, 0.125)',
+    '&:before': {
+        display: 'none',
+    },
+};
+
+export default function MenuAccordion() {
     return (
-        <List>
-            <MenuLink to="/best-under-18">
-                <StyledListItemButton>
-                    <MenuItemText className="menu-item-text">Under 18</MenuItemText>
-                </StyledListItemButton>
-            </MenuLink>
-            <MenuLink to="/best-under-19">
-                <StyledListItemButton>
-                    <MenuItemText className="menu-item-text">Under 19</MenuItemText>
-                </StyledListItemButton>
-            </MenuLink>
-            <MenuLink to="/best-under-20">
-                <StyledListItemButton>
-                    <MenuItemText className="menu-item-text">Under 20</MenuItemText>
-                </StyledListItemButton>
-            </MenuLink>
-            <MenuLink to="/best-under-21">
-                <StyledListItemButton>
-                    <MenuItemText className="menu-item-text">Under 21</MenuItemText>
-                </StyledListItemButton>
-            </MenuLink>
-            <MenuLink to="/best-under-22">
-                <StyledListItemButton>
-                    <MenuItemText className="menu-item-text">Under 22</MenuItemText>
-                </StyledListItemButton>
-            </MenuLink>
-            <MenuLink to="/best-under-23">
-                <StyledListItemButton>
-                    <MenuItemText className="menu-item-text">Under 23</MenuItemText>
-                </StyledListItemButton>
-            </MenuLink>
-        </List>
+        <Accordion defaultExpanded sx={accordionStyles}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="menu-content"
+                id="menu-header"
+            >
+                <Typography variant="h6">Giovani per Età</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <List>
+                    <MenuLink to="/best-under-18">
+                        <StyledListItemButton>
+                            <ListItemIcon>
+                                <SportsSoccer sx={{ color: '#fff' }} />
+                            </ListItemIcon>
+                            <MenuItemText primary="Under 18" />
+                        </StyledListItemButton>
+                    </MenuLink>
+                    <Divider />
+                    <MenuLink to="/best-under-19">
+                        <StyledListItemButton>
+                            <ListItemIcon>
+                                <People sx={{ color: '#fff' }} />
+                            </ListItemIcon>
+                            <MenuItemText primary="Under 19" />
+                        </StyledListItemButton>
+                    </MenuLink>
+                    <Divider />
+                    <MenuLink to="/best-under-20">
+                        <StyledListItemButton>
+                            <ListItemIcon>
+                                <School sx={{ color: '#fff' }} />
+                            </ListItemIcon>
+                            <MenuItemText primary="Under 20" />
+                        </StyledListItemButton>
+                    </MenuLink>
+                    <Divider />
+                    <MenuLink to="/best-under-21">
+                        <StyledListItemButton>
+                            <ListItemIcon>
+                                <Star sx={{ color: '#fff' }} />
+                            </ListItemIcon>
+                            <MenuItemText primary="Under 21" />
+                        </StyledListItemButton>
+                    </MenuLink>
+                    <Divider />
+                    <MenuLink to="/best-under-22">
+                        <StyledListItemButton>
+                            <ListItemIcon>
+                                <AccessAlarm sx={{ color: '#fff' }} />
+                            </ListItemIcon>
+                            <MenuItemText primary="Under 22" />
+                        </StyledListItemButton>
+                    </MenuLink>
+                    <Divider />
+                    <MenuLink to="/best-under-23">
+                        <StyledListItemButton>
+                            <ListItemIcon>
+                                <TrendingUp sx={{ color: '#fff' }} />
+                            </ListItemIcon>
+                            <MenuItemText primary="Under 23" />
+                        </StyledListItemButton>
+                    </MenuLink>
+                </List>
+            </AccordionDetails>
+        </Accordion>
     );
 }
