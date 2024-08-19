1c84d16e0513725f6acb1fe1a88227bd917afbf4
Michele Sifanno <michelesifanno@Air-di-Michele.homenet.telecomitalia.it>
2024-08-20 00:25:09 +0200

    test endopoint

--- src/SetupProxy.js
+++ src/SetupProxy.js
@@ -1,16 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  if (process.env.NODE_ENV === 'development') {
-    app.use(
-      '/api',
-      createProxyMiddleware({
-        target: 'https://www.fotmob.com',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '', // Rimuovi '/api' dalla richiesta
-        },
-      })
-    );
-  }
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://www.fotmob.com',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '', // Rimuovi '/api' dalla richiesta
+      },
+    })
+  );
 };
