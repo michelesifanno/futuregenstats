33735aadfcb88c603ccecf5475605ea3ab284331
Michele Sifanno <147255819+michelesifanno@users.noreply.github.com>
2024-08-28 14:35:40 +0200

    Delete backend/run-fetch-stats.js

--- backend/run-fetch-stats.js
+++
@@ -1,7 +0,0 @@
-const fetchStats = require('./fetchInfo');
-
-fetchStats().then(() => {
-  console.log('Stats fetched and stored successfully.');
-}).catch(error => {
-  console.error('Error fetching stats:', error);
-});
